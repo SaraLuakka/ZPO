38dfbfc5049e51a9d2e85232a9a99034e0454fd4
SaraLuakka <92623335+SaraLuakka@users.noreply.github.com>
2024-01-07 15:00:37 +0100

    Delete springbootwebapp-master/springbootwebapp-master directory

--- springbootwebapp-master/springbootwebapp-master/src/main/java/zpo/springframework/repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package zpo.springframework.repositories;
-
-import zpo.springframework.domain.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<Task, Integer>{
-}
